3f774d978a392cb464e5a986e0a49a0d896d060f
ganga0311 <86521494+ganga0311@users.noreply.github.com>
2021-09-29 10:25:30 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="clearfix mt-4">
+        <h1>LetsGrowMore</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
